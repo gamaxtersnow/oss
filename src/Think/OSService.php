5fadae5be12e54dd1dff8ed19f9f9291ffe388bd
wuzhaohui <wuchaohui@meishiedu.com>
2024-06-20 10:42:46 +0800

    fix

--- src/Think/OSService.php
+++ src/Think/OSService.php
@@ -8,9 +8,6 @@ class OSService extends Service
     public function register(): void
     {
         $config = config('oss');
-        if(empty($config)) {
-            $config = require __DIR__.'/oss.php';
-        }
         $this->app->bind('oss', new App($config));
     }
 
